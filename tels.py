55cf64b1028fca714262ea7e8515998d531ff840
thecsw <thecsmoothiew@yahoo.com>
2017-12-21 18:20:15 +0400

    Added main script

---
+++ tels.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+import os
+import telepot
+from telepot.loop import MessageLoop
+key = '461730776:AAHUE9Dr-_jxaZ3YZtub_zYQfu4oZVlSDyE'
+bot = telepot.Bot(key)
+
+ 
+def handle(msg):
+    user_id = msg['chat']['id']
+    command = msg['text'].encode('utf-8').lower() # Support lowercase
+    tts = gTTS(text=command, lang='en')
+    tts.save("{}.mp3".format(command))
+    bot.sendVoice(user_id, open("{}.mp3".format(command)))
+
+    try:
+        os.remove('{}.mp3'.format(command))
+    except:
+        pass
+MessageLoop(bot, handle).run_forever()
