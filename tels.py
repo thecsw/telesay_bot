a9a06cd2374ee8e02306027701a3e20310a052c0
thecsw <thecsmoothiew@yahoo.com>
2017-12-22 12:29:33 +0400

    Adding command helps

--- tels.py
+++ tels.py
@@ -8,6 +8,15 @@ bot = telepot.Bot(config.key)
 def handle(msg):
     user_id = msg['chat']['id']
     command = msg['text'].encode('utf-8').lower() # Support lowercase
+    if command == "/start":
+        bot.sendMessage(user_id, "Thank you for starting telesaybot!\n\
+Send any text to him and he will send you a computer-generated voice.")
+        return
+    
+    if command == "/help":
+        bot.sendMessage(user_id, "Send any text message and you will receive a computer-generated voice that reads your message.")
+        return
+    
     tts = gTTS(text=command, lang='en')
     tts.save("{}.mp3".format(command))
     bot.sendChatAction(user_id, 'upload_audio')
