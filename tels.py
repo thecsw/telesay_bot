615fe308afd10e091bbb7bc73542695dcf8aadfc
thecsw <sagindyk.urazayev@protonmail.ch>
2017-12-26 14:18:33 +0400

    Validation

--- tels.py
+++ tels.py
@@ -4,19 +4,30 @@ import telepot
 from telepot.loop import MessageLoop
 import config
 bot = telepot.Bot(config.key)
- 
+
+def say_exit(user_id, text):
+    bot.sendMessage(user_id, text)
+
 def handle(msg):
     user_id = msg['chat']['id']
     command = msg['text'].encode('utf-8').lower() # Support lowercase
     if command == "/start":
-        bot.sendMessage(user_id, "Thank you for starting telesaybot!\n\
+        say_exit(user_id, "Thank you for starting telesaybot!\n\
 Send any text to him and he will send you a computer-generated voice.")
         return
-    
+        
     if command == "/help":
-        bot.sendMessage(user_id, "Send any text message and you will receive a computer-generated voice that reads your message.")
+        say_exit(user_id, "Send any text message and you will receive a computer-generated voice that reads your message.")
         return
-
+        
+    if command[0] == '/':
+        say_exit(user_id, "Do not start message with a slash if it is not a command!")
+        return
+        
+    if len(command) > 128:
+        say_exit(user_id, "Too many characters. Limit = 128.")
+        return
+    
     bot.sendMessage(user_id, "Processing... Please wait.")
     tts = gTTS(text=command, lang='en')
     tts.save("{}.mp3".format(command))
